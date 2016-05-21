3dc56f54f72686df9a3c6b30835425bc124bdff0
Alex Genco <alexgenco@gmail.com>
2016-05-21 12:26:55 -0700

    Remove lifetimes

--- rust/hello-world/src/lib.rs
+++ rust/hello-world/src/lib.rs
@@ -1,4 +1,4 @@
-pub fn hello<'a>(name: Option<&'a str>) -> String {
+pub fn hello(name: Option<&str>) -> String {
     match name {
         Some(n) => format!("Hello, {}!", n),
         None => "Hello, World!".to_string()
