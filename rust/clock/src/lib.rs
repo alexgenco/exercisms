1a00b0a0318f61f04a68d4f9811f53d33db9cb46
Alex Genco <alexgenco@gmail.com>
2019-12-08 10:56:22 -0800

    fixup clock based on feedback

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,28 +1,42 @@
-#[derive(PartialEq, Debug)]
+use std::fmt;
+
+#[derive(Debug, Eq)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
 
-impl ToString for Clock {
-    fn to_string(&self) -> String {
-        format!("{:02}:{:02}", self.hours, self.minutes)
+impl Clock {
+    fn hour(&self) -> i32 {
+        self.minutes.wrapping_div_euclid(60).wrapping_rem_euclid(24)
+    }
+
+    fn minute(&self) -> i32 {
+        self.minutes.wrapping_rem_euclid(60)
+    }
+}
+
+impl PartialEq for Clock {
+    fn eq(&self, other: &Self) -> bool {
+        self.to_string() == other.to_string()
+    }
+}
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:02}:{:02}", self.hour(), self.minute())
     }
 }
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let total = hours * 60 + minutes;
-        let wrapped_mins = total.wrapping_rem_euclid(60);
-        let wrapped_hrs = total.wrapping_div_euclid(60).wrapping_rem_euclid(24);
-
-        Clock {
-            hours: wrapped_hrs,
-            minutes: wrapped_mins,
+        Self {
+            minutes: hours * 60 + minutes,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        Self::new(self.hours, self.minutes + minutes)
+        Self {
+            minutes: self.minutes + minutes,
+        }
     }
 }
